597e99174b10ec924c2e8a66685c282f005b6895
Noela-Murugi <murugin254@gmail.com>
2022-08-25 12:45:00 +0300

    completed the assignment

--- main.js
+++ main.js
@@ -3,9 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+const glyphHearts = document.querySelectorAll(".like-glyph");
+function activateHeart (e) {
+  const heartLike = e.target;
+  mimicServerCall()
+  .then(() => {
+    if(heartLike.textContent === EMPTY_HEART) {
+      heartLike.textContent = FULL_HEART;
+    } else {
+      heartLike.textContent = EMPTY_HEART;
+    }
+  })
+  .catch(error => {
+    let modalError = document.querySelector("#modal");
+    modalError.innerText = error;
+    setTimeout(() => modalError.className = "hidden", 3000);
+  });
+}
+function eventHeart(glyphHearts){
+  for (const heart of glyphHearts) {
+      heart.addEventListener('click', activateHeart);
+    }
+}
+eventHeart(glyphHearts);
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
